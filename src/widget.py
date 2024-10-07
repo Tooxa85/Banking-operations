c875ab27c23938beb8e64c80a7e17bd441ac5193
Tooxa85 <tooxa85@gmail.ru>
2024-10-07 08:27:39 +0300

    add function

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(nums: str) -> str | None:
+    """Функция маскировки карты и счета"""
+    if "Счет" in nums:
+        return "Счет " + get_mask_account(nums)
+    else:
+        cards = get_mask_card_number(nums[-16:])
+        new_card = nums.replace(nums[-16:], cards)
+        return new_card
+
+
+def get_date(date: str) -> str | None:
+    """Функция преобразовывает дату в формате 'dd.mm.yy'"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
